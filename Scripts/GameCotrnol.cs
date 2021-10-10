a03e45d19ff27259bab6e3470b16f01c03bad9ac
Your Name <you@example.com>
2021-10-10 14:59:38 +0300

    Simple-Game

---
+++ Scripts/GameCotrnol.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameCotrnol : MonoBehaviour {
+
+    public static GameCotrnol instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+	void Awake () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy (gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if ( gameOver == true && Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);// Please reload whatever its open atm
+        }
+	}
+    public void PlayerDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
---
+++ Scripts/PlayerCtrl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+    public GameObject gameOverText;
+    public float RunSpeed;
+    private bool isDead = false;
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isDead == false)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                //transform.position += Vector3.left * speed * Time.deltaTime; // Shorthand of the line below
+                transform.position = new Vector3(transform.position.x - RunSpeed *
+                    Time.deltaTime, transform.position.y + RunSpeed *
+                    Time.deltaTime, transform.position.z);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = new Vector3(transform.position.x + RunSpeed *
+                    Time.deltaTime, transform.position.y + RunSpeed *
+                    Time.deltaTime, transform.position.z);
+            }
+        
+        }
+    }
+  void OnCollisionEnter2D()
+    {
+        isDead = true;
+        GameCotrnol.instance.PlayerDied();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coins"))// this is for destroing the Coins from the game if the colliders hit
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
